17066d5aa9aba3d6c5381322df4cdeb36b88755d
lyoun009 <47126840+lyoun009@users.noreply.github.com>
2021-06-11 14:37:26 -0700

    fixed retrievedoc based on keyword issue, modified elasticsearch commands

--- indexer.sh
+++ indexer.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-echo "Commands to run indexer:"
-echo
-echo "i - create index"
-echo "c - check status (health) of instance"
-echo "s - put in single document"
-echo "a - retrieve all documents"
-echo "r - retrieve documents with specific word"
-echo "b - bulk load documents via JSON"
-echo "d - delete entire index"
-echo "q - quit"
+echo "Please enter name of index:"
+read index
 
-
-read option
-while [[ [[$option != 'q']] ]]
-do
-    if [[ $option == 'i' ]]; then
-        echo "Creating index called cs172index..."
         curl -X PUT -u elastic:9GObQ236qhyZLDb3gbccg0fx "https://cs172-b03cf7.es.us-west1.gcp.cloud.es.io:9243/cs172index?pretty" -H 'Content-Type: application/json' -d'{
         "settings": {
             "analysis": {
@@ -37,21 +23,44 @@ do
                 }
             }
         }
-        }'
+        }' &> log.txt
 
-        
-    elif [[ $option == 'c' ]]; then
+echo
+if grep -Fxq "already exists" log.txt
+then
+    echo "${index} doesn't exist, created new index, ${index}"
+else
+    echo "${index} already exists, accessing ${index}"
+fi
+
+echo
+echo "Commands to run indexer:"
+echo
+
+echo "c - check status (health) of instance"
+echo "s - put in single document"
+echo "a - retrieve all documents"
+echo "r - retrieve documents with specific word"
+echo "b - bulk load documents via JSON"
+echo "d - delete all documents in index"
+echo "q - quit"
+
+
+read option
+while [[ [[$option != 'q']] ]]
+do
+    if [[ $option == 'c' ]]; then
         echo "Checking health of cluster..."
         curl -X GET -u elastic:9GObQ236qhyZLDb3gbccg0fx "https://cs172-b03cf7.es.us-west1.gcp.cloud.es.io:9243/_cluster/health?pretty"
         
     elif [[ $option == 's' ]]; then
         echo "Please enter in a document (ie. like \"<td><tr>test<td </tr>\" )"
         read doc
-        curl -X PUT -u elastic:9GObQ236qhyZLDb3gbccg0fx "https://cs172-b03cf7.es.us-west1.gcp.cloud.es.io:9243/cs172index/_doc/1" -H 'Content-Type: application/json' -d'{"html": "${doc}"}'
+        curl -X PUT -u elastic:9GObQ236qhyZLDb3gbccg0fx "https://cs172-b03cf7.es.us-west1.gcp.cloud.es.io:9243/${index}/_doc/1" -H 'Content-Type: application/json' -d'{"html": "${doc}"}'
         
     elif [[ $option == 'a' ]]; then
-        echo "Retrieving all documents..."
-        curl -X GET -u elastic:9GObQ236qhyZLDb3gbccg0fx "https://cs172-b03cf7.es.us-west1.gcp.cloud.es.io:9243/cs172index/_search?pretty" -H 'Content-Type: application/json' -d'
+        echo "Retrieving all documents from ${index}..."
+        curl -X GET -u elastic:9GObQ236qhyZLDb3gbccg0fx "https://cs172-b03cf7.es.us-west1.gcp.cloud.es.io:9243/${index}/_search?pretty" -H 'Content-Type: application/json' -d'
         {
         "query": {
             "match_all": { }
@@ -62,29 +71,30 @@ do
     elif [[ $option == 'r' ]]; then
         echo "Please enter a word you would like to retrieve by (ie. \"ucr\"): "
         read word
-        curl -X GET -u elastic:9GObQ236qhyZLDb3gbccg0fx "https://cs172-b03cf7.es.us-west1.gcp.cloud.es.io:9243/cs172index/_search?pretty" -H 'Content-Type: application/json' -d'
-        {
-        "query": {
-            "match": {
-            "html": "${word}"
-            }
-        }
-        }'
+                                                                                                                                                                               
+        curl -X GET -u elastic:9GObQ236qhyZLDb3gbccg0fx "https://cs172-b03cf7.es.us-west1.gcp.cloud.es.io:9243/${index}/_search?pretty" -H 'Content-Type: application/json' -d "{\"query\": {\"match\": {\"html\":\" $word \"}}}"
         
     elif [[ $option == 'b' ]]; then
         echo "Creating JSON file to load..."
         python3 create_json.py
-        echo "Bulk loading documents..."
-        curl -X POST -u elastic:9GObQ236qhyZLDb3gbccg0fx "https://cs172-b03cf7.es.us-west1.gcp.cloud.es.io:9243/cs172index/_bulk" -H "Content-Type: application/x-ndjson" --data-binary @data.json
+        echo "Bulk loading documents into ${index}..."
+        curl -X POST -u elastic:9GObQ236qhyZLDb3gbccg0fx "https://cs172-b03cf7.es.us-west1.gcp.cloud.es.io:9243/${index}/_bulk" -H "Content-Type: application/x-ndjson" --data-binary @data.json
         
     elif [[ $option == 'd' ]]; then
-        echo "Please enter the name of the index you wish to delete:"
-        read index
-        echo "Deleting ${index}..."
-        curl -X DELETE -u elastic:9GObQ236qhyZLDb3gbccg0fx "https://cs172-b03cf7.es.us-west1.gcp.cloud.es.io:9243/${index}"
-        
+        echo "Deleting all documents in ${index}..."
+        curl -X POST -u elastic:9GObQ236qhyZLDb3gbccg0fx "https://cs172-b03cf7.es.us-west1.gcp.cloud.es.io:9243/${index}/_delete_by_query?pretty" -H 'Content-Type: application/json' -d'
+        {
+        "query": {
+            "match_all": {
+            
+            }
+        }
+        }
+        '
+
     elif [[ $option == 'q' ]]; then
         echo "Quiting..."
+        rm log.txt
         break
     else
     echo "not valid option, try again"
@@ -94,13 +104,12 @@ do
     echo
     echo "Commands to run indexer:"
     echo
-    echo "i - create index"
     echo "c - check status (health) of instance"
     echo "s - put in single document"
     echo "a - retrieve all documents"
     echo "r - retrieve documents with specific word"
     echo "b - bulk load documents via JSON"
-    echo "d - delete entire index"
+    echo "d - delete all documents in index"
     echo "q - quit"
     read option
 done
