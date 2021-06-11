9d875849a475c4dd5585d58c7322a909eee05cbc
lyoun009 <47126840+lyoun009@users.noreply.github.com>
2021-06-10 22:22:14 -0700

    json bulk add, works with elasticsearch

--- create_json.py
+++ create_json.py
@@ -4,10 +4,11 @@ import json
 
 files = glob.glob('htmls/*')
 os.system("rm data.json")
+count = 1
 for f in files:
     with open(f, "r") as html_file:
         html = html_file.read()
-        x = {"index": "{}"}
+        x = {"index": {"_id": "" + str(count) + ""}}
         x2 = {"html": html}   
         
     with open("data.json", "a") as file:
@@ -15,4 +16,9 @@ for f in files:
         file.write("\n")
         json.dump(x2, file)
         file.write("\n")
+    count += 1
+
+file = open("data.json", "a")
+file.write('\n')
+file.close()
       
